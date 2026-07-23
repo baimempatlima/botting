f6ade03a7b4971fb6e24c1473f3c3e1713826e02
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2026-07-24 01:28:54 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Masukkan konfigurasi yang Anda butuhkan di sini (misalnya konfigurasi gambar Supabase sebelumnya)
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'obwlmuyvcrzbhjbwrxca', // Ganti dengan ID project Supabase Anda (jika pakai)
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-// Gunakan 'export default' untuk file berakhiran .mjs
 export default nextConfig;
